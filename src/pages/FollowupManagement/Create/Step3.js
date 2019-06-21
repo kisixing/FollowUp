a759dc2d1884be3e85c09d6809e47a20f494a059
lxf <lixiaofan.dev@gmail.com>
2019-06-21 18:13:23 +0800

    随访任务内容编辑及发布页

--- src/pages/FollowupManagement/Create/Step3.js
+++ src/pages/FollowupManagement/Create/Step3.js
@@ -1,20 +1,256 @@
-/*
- * @Description: 步骤二 选择对象
- * @Author: zhong jun
- * @Date: 2019-06-20 17:30:45
- */
+import {
+  Tabs,
+  Button,
+  Dropdown,
+  Menu,
+  Icon,
+  Form,
+  Input,
+  TimePicker,
+  Radio,
+  Switch,
+  Checkbox,
+  Col,
+  Row,
+} from 'antd';
+import { lh40, colorC, mRb8 } from './Step3.less';
+import router from 'umi/router';
+const { TabPane } = Tabs;
+const reservationDateType = ['预约日期', '末次就诊日期'];
+const reservationDuringType = ['之前', '当天', '之后'];
+const reservationMediaType = ['微信', '短信', '电话'];
+const { useState } = React;
+export default class Demo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.newTabIndex = 0;
+    const panes = [
+      { title: '步骤1', content: <Content1 />, key: '1' },
+      { title: '步骤2', content: 'Content of Tab Pane 2', key: '2' },
+    ];
+    this.state = {
+      activeKey: panes[0].key,
+      panes,
+    };
+  }
 
-import React, { Component } from 'react';
+  onChange = activeKey => {
+    this.setState({ activeKey });
+  };
 
-export class Step3 extends Component {
+  onEdit = (targetKey, action) => {
+    this[action](targetKey);
+  };
+
+  add = () => {
+    const panes = this.state.panes;
+    const activeKey = `newTab${this.newTabIndex++}`;
+    panes.push({ title: 'New Tab', content: 'New Tab Pane', key: activeKey });
+    this.setState({ panes, activeKey });
+  };
+
+  remove = targetKey => {
+    let activeKey = this.state.activeKey;
+    let lastIndex;
+    this.state.panes.forEach((pane, i) => {
+      if (pane.key === targetKey) {
+        lastIndex = i - 1;
+      }
+    });
+    const panes = this.state.panes.filter(pane => pane.key !== targetKey);
+    if (panes.length && activeKey === targetKey) {
+      if (lastIndex >= 0) {
+        activeKey = panes[lastIndex].key;
+      } else {
+        activeKey = panes[0].key;
+      }
+    }
+    this.setState({ panes, activeKey });
+  };
 
   render() {
     return (
       <div>
-        step 2
+        <Tabs
+          // hideAdd={false}
+          onChange={this.onChange}
+          activeKey={this.state.activeKey}
+          type="editable-card"
+          onEdit={this.onEdit}
+        >
+          {this.state.panes.map(pane => (
+            <TabPane tab={pane.title} key={pane.key}>
+              {pane.content}
+            </TabPane>
+          ))}
+        </Tabs>
       </div>
-    )
+    );
   }
 }
 
-export default Step3;
+function handleButtonClick(e) {
+  message.info('Click on left button.');
+  console.log('click left button', e);
+}
+
+function handleMenuClick(e) {
+  debugger;
+  message.info('Click on menu item.');
+  console.log('click', e);
+}
+
+const menu = (
+  <Menu onClick={handleMenuClick}>
+    <Menu.Item key="1">1st menu item</Menu.Item>
+    <Menu.Item key="2">2nd menu item</Menu.Item>
+  </Menu>
+);
+
+function Content1(props) {
+  const [formData, setFormData] = useState({
+    dateType: reservationDateType[0],
+    duringType: reservationDuringType[0],
+    mediaType: reservationMediaType[0],
+    IsfollowOrder: false,
+  });
+
+  function _setFormData(key, value) {
+    setFormData({ ...formData, [key]: value });
+  }
+  const { dateType, duringType, mediaType, IsfollowOrder } = formData;
+
+  function getDropDown(type, typeList) {
+    return (
+      <Dropdown overlay={getMenu(typeList, ({ key }) => _setFormData(type, key))}>
+        <Button>
+          {formData[type]} <Icon type="down" />
+        </Button>
+      </Dropdown>
+    );
+  }
+
+  return (
+    <Form
+      layout="horizontal"
+      labelCol={{ span: 2, offset: 1 }}
+      wrapperCol={{ span: 14 }}
+      labelAlign="left"
+    >
+      <Title label="随访时间&媒介" isTop />
+      <Form.Item label="时间">
+        <Input.Group compact style={{ lineHeight: '32px' }}>
+          {getDropDown('dateType', reservationDateType)}
+          {getDropDown('duringType', reservationDuringType)}
+          <Input style={{ width: '60px' }} /> 天 <TimePicker />
+        </Input.Group>
+      </Form.Item>
+
+      <Form.Item label="选择媒介">
+        <Radio.Group
+          value={mediaType}
+          onChange={({ target }) => {
+            _setFormData('mediaType', target.value);
+          }}
+        >
+          {reservationMediaType.map(r => (
+            <Radio.Button value={r} key={r}>
+              {r}
+            </Radio.Button>
+          ))}
+        </Radio.Group>
+      </Form.Item>
+      <Form.Item label=" " colon={false}>
+        <label style={{ marginRight: 10 }}>
+          选择媒介没发送成功，将按微信>短信>电话的先后顺序依次执行随访任务
+        </label>
+        <Switch
+          checked={IsfollowOrder}
+          onChange={IsfollowOrder => {
+            _setFormData('IsfollowOrder', IsfollowOrder);
+          }}
+        ></Switch>
+      </Form.Item>
+
+      <Title label="判断条件" />
+
+      <Form.Item label="条件">
+        <Input.Group compact style={{ lineHeight: '32px' }}>
+          {getDropDown('dateType', reservationDateType)}
+          {getDropDown('duringType', reservationDuringType)}
+          <Input style={{ width: '60px' }} /> 天 至 随访当天仍未复诊患者
+        </Input.Group>
+      </Form.Item>
+
+      <Title label="随访内容" />
+
+      <Form.Item label="提示文字">
+        <Input.TextArea
+          style={{ width: '80%' }}
+          placeholder="Autosize height with minimum and maximum number of lines"
+          autosize={{ minRows: 2, maxRows: 6 }}
+        />
+        <Button type="link" style={{ float: 'none' }}>
+          导入模板
+        </Button>
+        <div>
+          您可以在提示文字中插入：
+          {['孕妇姓名', '复诊预约时间', ' 超时天数'].map(_ => {
+            return (
+              <Button type="link" key={_}>
+                {_}
+              </Button>
+            );
+          })}
+        </div>
+      </Form.Item>
+      <Row>
+        <Col offset={1}>
+          <div className={lh40}>
+            <Checkbox> 附加链接 </Checkbox>
+          </div>
+          <div className={lh40}>
+            <Radio> 预约挂号 </Radio>
+            <label className={colorC}>提示：仅微信推送的消息可附加</label>
+          </div>
+          <div className={lh40}>
+            <Radio> 绑定问卷 </Radio>
+            <Button type="primary" ghost>
+              未及时就诊原因
+            </Button>
+            <Button type="link">其他问卷</Button>
+          </div>
+        </Col>
+      </Row>
+      <div style={{ textAlign: 'center' }}>
+        <Button className={mRb8}>预览</Button>
+        <Button type="primary" className={mRb8}>
+          上一步
+        </Button>
+        <Button type="primary" className={mRb8}>
+          下一步
+        </Button>
+        <Button type="primary" className={mRb8} onClick={() => router.push('Step4')}>
+          确定
+        </Button>
+      </div>
+    </Form>
+  );
+}
+export function Title({ label, isTop }) {
+  return (
+    <div style={{ fontWeight: 'bold', marginTop: isTop || '30px', marginBottom: '10px' }}>
+      {label}
+    </div>
+  );
+}
+
+function getMenu(arr, handleMenuClick) {
+  return (
+    <Menu onClick={handleMenuClick}>
+      {arr.map(a => (
+        <Menu.Item key={a}>{a}</Menu.Item>
+      ))}
+    </Menu>
+  );
+}
--- src/pages/FollowupManagement/Create/Step4.js
+++ src/pages/FollowupManagement/Create/Step4.js
@@ -1,20 +1,245 @@
-/*
- * @Description: 步骤二 选择对象
- * @Author: zhong jun
- * @Date: 2019-06-20 17:30:45
- */
+import {
+  Transfer,
+  Modal,
+  Tabs,
+  Button,
+  Dropdown,
+  Menu,
+  Icon,
+  Form,
+  Input,
+  TimePicker,
+  Radio,
+  Switch,
+  Checkbox,
+  Col,
+  Row,
+} from 'antd';
+const CheckboxGroup = Checkbox.Group;
 
-import React, { Component } from 'react';
+import { lh40, colorC, mRb8 } from './Step3.less';
+import router from 'umi/router';
+const { TabPane } = Tabs;
+const reservationDateType = ['预约日期', '末次就诊日期'];
+const reservationDuringType = ['之前', '当天', '之后'];
+const filterType = ['复诊预约时间段', '超时天数', '高危等级', '跟踪结果'];
+const taskTableContent = setMock([
+  '姓名',
+  '就诊卡号',
+  '手机号',
+  '现孕周',
+  '高危等级',
+  '上次就诊时间',
+  '复诊预约时间',
+  '超时天数',
+  '短信回执',
+  '电话状态',
+  '电话随访记录超时原因',
+  '电话随访备注',
+]);
+const statisticTableContent = setMock([
+  '姓名',
+  '就诊卡号',
+  '手机号',
+  '孕周',
+  '高危等级',
+  '上次就诊时间',
+  '复诊预约时间',
+  '按时复诊',
+  '跟踪结果',
+  '实际复诊时间',
+  '追踪方式',
+  '电话状态',
+]);
 
-export class Step4 extends Component {
-
-  render() {
-    return (
-      <div>
-        step 2
-      </div>
-    )
-  }
+function setMock(arr) {
+  return arr.map(a => {
+    return {
+      key:
+        a +
+        Math.random()
+          .toString(16)
+          .slice(2),
+      title: a,
+      // description: `description of content${a}`,
+      // chosen:true
+    };
+  });
 }
 
-export default Step4;
+const { useState } = React;
+import { Title } from './Step3';
+
+export default function(props) {
+  const [state, setState] = useState({
+    dateType: reservationDateType[0],
+    duringType: reservationDuringType[0],
+    IsfollowOrder: false,
+    taskVisible: false,
+    taskTargetKeys: [],
+    _taskTargetKeys: [],
+    statisticTargetKeys: [],
+    _statisticTargetKeys: [],
+  });
+
+  function _setFormData(key, value) {
+    setState({ ...state, [key]: value });
+  }
+  const {
+    dateType,
+    duringType,
+    IsfollowOrder,
+    taskVisible,
+    taskTargetKeys,
+    _taskTargetKeys,
+  } = state;
+
+  function getDropDown(type, typeList) {
+    return (
+      <Dropdown overlay={getMenu(typeList, ({ key }) => _setFormData(type, key))}>
+        <Button>
+          {state[type]} <Icon type="down" />
+        </Button>
+      </Dropdown>
+    );
+  }
+
+  function DirtyTransferModal(type) {
+    return (
+      <TransferModal
+        visible={`${type}Visible`}
+        dataSource={taskTableContent}
+        targetKeys={_taskTargetKeys}
+        onOk={() => {
+          setState({ ...state, taskTargetKeys: _taskTargetKeys, taskVisible: false });
+        }}
+        onCancel={() => setState({ ...state, _taskTargetKeys: [], taskVisible: false })}
+        onChange={targetkeys => {
+          setState({ ...state, _taskTargetKeys: targetkeys });
+        }}
+      />
+    );
+  }
+
+  return (
+    <Form
+      layout="horizontal"
+      labelCol={{ span: 2, offset: 1 }}
+      wrapperCol={{ span: 21 }}
+      labelAlign="left"
+    >
+      <Title label="任务管理" isTop />
+      <Row>
+        <Col offset={1}>
+          <div className={lh40}>
+            <Checkbox>人工管理</Checkbox>
+          </div>
+        </Col>
+      </Row>
+      <Form.Item label="对象范围">
+        <Input.Group compact style={{ lineHeight: '32px' }}>
+          {getDropDown('dateType', reservationDateType)}
+          {getDropDown('duringType', reservationDuringType)}
+          <Input style={{ width: '60px' }} /> 天起 仍未复诊孕妇
+        </Input.Group>
+      </Form.Item>
+
+      <Form.Item label="筛选条件">{CheckboxList({ list: filterType })}</Form.Item>
+
+      <Form.Item label="表格内容">
+        <Button
+          type="link"
+          style={{ whiteSpace: 'normal', textAlign: 'left' }}
+          onClick={() => setState({ ...state, taskVisible: true })}
+        >
+          {taskTargetKeys.length > 0 ? (
+            taskTableContent
+              .filter(t => taskTargetKeys.includes(t.key))
+              .map(t => t.title)
+              .join('、')
+          ) : (
+            <a>添加</a>
+          )}
+        </Button>
+        <TransferModal
+          visible={taskVisible}
+          dataSource={taskTableContent}
+          targetKeys={_taskTargetKeys}
+          onOk={() => {
+            setState({ ...state, taskTargetKeys: _taskTargetKeys, taskVisible: false });
+          }}
+          onCancel={() => setState({ ...state, _taskTargetKeys: [], taskVisible: false })}
+          onChange={targetkeys => {
+            setState({ ...state, _taskTargetKeys: targetkeys });
+          }}
+        />
+      </Form.Item>
+      <div style={{ textAlign: 'center' }}>
+        <Button className={mRb8}>预览</Button>
+        <Button type="primary" className={mRb8} onClick={() => router.push('Step4')}>
+          发布
+        </Button>
+      </div>
+    </Form>
+  );
+}
+
+function TransferModal({ visible, onOk, onCancel, targetKeys, dataSource, onChange }) {
+  return (
+    <Modal visible={visible} onCancel={onCancel} onOk={onOk}>
+      <Transfer
+        targetKeys={targetKeys}
+        dataSource={dataSource}
+        render={item => item.title}
+        onChange={onChange}
+      ></Transfer>
+    </Modal>
+  );
+}
+
+function getMenu(arr, handleMenuClick) {
+  return (
+    <Menu onClick={handleMenuClick}>
+      {arr.map(a => (
+        <Menu.Item key={a}>{a}</Menu.Item>
+      ))}
+    </Menu>
+  );
+}
+
+function CheckboxList({ list }) {
+  const [state, setState] = useState({
+    checkedList: [],
+    indeterminate: false,
+    checkAll: false,
+  });
+
+  const onChange = checkedList => {
+    setState({
+      checkedList,
+      indeterminate: !!checkedList.length && checkedList.length < list.length,
+      checkAll: checkedList.length === list.length,
+    });
+  };
+
+  const onCheckAllChange = e => {
+    setState({
+      checkedList: e.target.checked ? list : [],
+      indeterminate: false,
+      checkAll: e.target.checked,
+    });
+  };
+
+  return (
+    <div>
+      <CheckboxGroup options={list} value={state.checkedList} onChange={onChange} />
+      <Checkbox
+        indeterminate={state.indeterminate}
+        onChange={onCheckAllChange}
+        checked={state.checkAll}
+      >
+        全选
+      </Checkbox>
+    </div>
+  );
+}
