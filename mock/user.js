c165ca4c18cb33908dd7ee00e559e0b6ba8b3308
lxf <lixiaofan.dev@gmail.com>
2019-06-28 09:37:24 +0800

    登录页 logo 更换主页；用户名改为 管理员，并去掉头像；右侧配置按钮隐藏

--- mock/user.js
+++ mock/user.js
@@ -2,7 +2,7 @@
 export default {
   // 支持值为 Object 和 Array
   'GET /api/currentUser': {
-    name: 'Serati Ma',
+    name: '管理员',
     avatar: 'https://gw.alipayobjects.com/zos/antfincdn/XAosXuNZyF/BiazfanxmamNRoxxVxka.png',
     userid: '00000001',
     email: 'antdesign@alipay.com',
--- src/components/GlobalHeader/RightContent.js
+++ src/components/GlobalHeader/RightContent.js
@@ -175,12 +175,12 @@ export default class GlobalHeaderRight extends PureComponent {
         {currentUser.name ? (
           <HeaderDropdown overlay={menu}>
             <span className={`${styles.action} ${styles.account}`}>
-              <Avatar
+              {/* <Avatar
                 size="small"
                 className={styles.avatar}
                 src={currentUser.avatar}
                 alt="avatar"
-              />
+              /> */}
               <span className={styles.name}>{currentUser.name}</span>
             </span>
           </HeaderDropdown>
